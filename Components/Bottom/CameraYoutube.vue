7f3826b43774eeeb1a5e7f52a1da4a2edd682b73
Gerard Llorach <gerard.llorach@gmail.com>
2025-03-13 12:04:46 +0100

    updated camera link. fixes #3

--- Components/Bottom/CameraYoutube.vue
+++ Components/Bottom/CameraYoutube.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="camera-youtube">
-    <iframe width="300" height="200" src="https://www.youtube.com/embed/TvJ6Yh5_W4A?controls=0&autoplay=1&showinfo=0">
+    <iframe width="300" height="200" src="https://www.youtube.com/embed/o_nEsj6EETw?si=oW4SM83oZEzhj3YZ">
     </iframe>
   </div>
 </template>
@@ -9,5 +9,6 @@
 #camera-youtube{
     display: flex;
     justify-content: flex-end;
+    pointer-events: all;
   }
 </style>
